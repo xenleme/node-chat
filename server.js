d69d4df353bf953337d43a5c47fc25b72ba4f0c6
xenleme <xenleme@gmail.com>
2019-08-03 15:03:04 +0300

    Added socket.io, http and refactored the code of the server

--- server.js
+++ server.js
@@ -1,22 +1,62 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
-const server = app.listen(3000, () => {
-  console.log('sever is running on port', server.address().port);
-});
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const mongoose = require('mongoose');
+const Message = mongoose.model('Message', {
+  name: String,
+  message: String
+});
+
 const dbUrl =
   'mongodb+srv://pavelb:<password>@chat-snezt.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(dbUrl, err => {
-  console.err('mongodb connected', err);
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
 });
 
-const Message = mongoose.model('Message', { name: String, message: String });
+app.get('/messages/:user', (req, res) => {
+  const user = req.params.user;
+  Message.find({ name: user }, (err, messages) => {
+    res.send(messages);
+  });
+});
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/messages', async (req, res) => {
+  try {
+    const message = new Message(req.body);
+
+    const savedMessage = await message.save();
+    console.log('Saved');
+
+    const censored = await Message.findOne({ message: 'Badword' });
+    if (censored) await Message.remove({ _id: censored.id });
+    else io.emit('Message', req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+    return console.log('Error', error);
+  } finally {
+    console.log('Message posted');
+  }
+});
+
+io.on('connection', () => {
+  console.log('User is connected');
+});
+
+mongoose.connect(dbUrl, { useNewUrlParser: true }, err => {
+  console.log('Mongodb connected', err);
+});
+
+const server = http.listen(3000, () => {
+  console.log('Server is running on port', server.address().port);
+});
